3ae7e08cb2fdb93a412fbb3ead052c4b9974e9bb
barrera-daniel-jpg <barreradb77@gmail.com>
2026-03-30 10:31:56 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    print("=" * 35)
+    print("INICIANDO EL SISTEMA...")
+    # Hace que se ejecute todo desde el main
+
+from interaccion import *
+loop() # Llama a la funcion nombrada
